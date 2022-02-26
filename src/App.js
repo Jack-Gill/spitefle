59c09998fbcbfea814d0d1bb8f983381f6773f24
Jack Gill <jackgill@trustedhousesitters.com>
2022-02-26 13:45:58 +0000

    Completes first draft, gets result generated, renders it, lets you copy it.

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import { useState } from "react";
+
+import "./App.css";
+import utils from "./utils";
+import { Attempt } from "./components";
+
+function App() {
+  const generate = () => {
+    let then = new Date("06/19/2021");
+    let now = new Date();
+    let difference = now.getTime() - then.getTime();
+    let days = Math.ceil(difference / (1000 * 3600 * 24)) - 1;
+
+    let random = Math.floor(Math.random() * 100);
+
+    let attempts;
+    let corrects = 4;
+
+    if (random < 5) attempts = 1;
+    else if (random < 15) attempts = 2;
+    else if (random < 35) attempts = 3;
+    else if (random < 75) attempts = 4;
+    else if (random < 90) attempts = 5;
+    else if (random < 97) attempts = 6;
+    else attempts = 7;
+
+    function getLetter() {
+      let random = Math.floor(Math.random() * 100);
+      if (corrects > 0) {
+        if (random < 50) return "⬛";
+        else if (random < 70) {
+          corrects = corrects - 1;
+          return "🟩";
+        }
+        return "🟨";
+      }
+
+      if (random < 30) return "⬛";
+      return "🟨";
+    }
+
+    let attemptsString;
+    let attemptsMap = [];
+
+    for (
+      let attemptsIndex = 0;
+      attemptsIndex < attempts - 1;
+      attemptsIndex += 1
+    ) {
+      const letters = [];
+      for (
+        let letterIndex = 0;
+        letterIndex < 5;
+        letterIndex = letterIndex + 1
+      ) {
+        const previousAttempt = attemptsMap[attemptsIndex - 1];
+        console.log(previousAttempt);
+        if (previousAttempt && previousAttempt[letterIndex] === "🟩") {
+          letters.push("🟩");
+        } else {
+          letters.push(getLetter());
+        }
+      }
+      attemptsMap.push(letters);
+    }
+
+    if (attempts < 7) {
+      attemptsString = attempts.toString();
+      attemptsMap.push(["🟩", "🟩", "🟩", "🟩", "🟩"]);
+    } else {
+      attemptsString = "X";
+    }
+
+    console.log(attemptsMap);
+
+    const title = `Wordle ${days} ${attemptsString}/6`;
+    let resultAsString = title + "\n";
+
+    attemptsMap.forEach((attempt) => {
+      resultAsString = resultAsString + "\n" + attempt.join("");
+    });
+
+    return {
+      title,
+      resultAsString,
+      attemptsMap,
+    };
+  };
+
+  const [result, setResult] = useState(generate());
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{result.title}</p>
+        {result.attemptsMap.map((attempt) => {
+          return <Attempt>{attempt}</Attempt>;
+        })}
+        <br />
+        <button onClick={() => setResult(generate())}>Regenerate</button>
+        <br />
+        <button
+          onClick={() => utils.copyTextToClipboard(result.resultAsString)}
+        >
+          Copy To Clipboard
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Attempt = styled.p`
+  margin: 0px;
+`;
+
+export { Attempt };
